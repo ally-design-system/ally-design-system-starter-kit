75f100ff203b0e21edcfa84bde538b669d7f5359
Nikki Pantony <nikkipantony@gmail.com>
2020-05-14 15:18:21 +0100

    Move styles files to 09_styles + re-structure, Update comments in styles files

--- src/04_templates/main-layout-template.js
+++ src/04_templates/main-layout-template.js
@@ -1,12 +1,12 @@
 /*
- * Main Site Layout Component
+ * Main Layout Template
  */
 
 import React from "react"
-import "../styles/main.scss"
-import { SkipNavLink, SkipNavContent } from "@reach/skip-nav"
+import "../09_styles/styles.scss"
 import Header from "../03_organisms/header"
 import Footer from "../03_organisms/footer"
+import { SkipNavLink, SkipNavContent } from "@reach/skip-nav"
 
 const MainLayout = ({ children }) => (
     <>
