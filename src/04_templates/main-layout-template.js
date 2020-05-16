090926d6bee476c5c8c1bafceac1921dfbb50ae0
Nikki Pantony <nikkipantony@gmail.com>
2020-05-16 18:34:06 +0100

    Rename folders to include 08_icons

--- src/04_templates/main-layout-template.js
+++ src/04_templates/main-layout-template.js
@@ -3,7 +3,7 @@
  */
 
 import React from "react"
-import "../09_styles/styles.scss"
+import "../10_styles/styles.scss"
 import Header from "../03_organisms/header"
 import Footer from "../03_organisms/footer"
 import { SkipNavLink, SkipNavContent } from "@reach/skip-nav"
