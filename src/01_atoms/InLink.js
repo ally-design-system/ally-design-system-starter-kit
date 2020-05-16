0b696645610a787124ce41be05954a4f122276a5
Nikki Pantony <nikkipantony@gmail.com>
2020-05-16 17:32:43 +0100

    Update code snippets, Add InLink component for internal links

--- src/01_atoms/InLink.js
+++ src/01_atoms/InLink.js
@@ -4,18 +4,25 @@
  */
 
 import React from "react"
+import { Link as GatsbyLink } from "gatsby"
 import PropTypes from "prop-types"
 
 const InLink = props => {
-    return <>{/* <p>{props.propName}</p> */}</>
+    return (
+        <>
+            <GatsbyLink to={props.to}>{props.text}</GatsbyLink>
+        </>
+    )
 }
 
 InLink.defaultProps = {
-    // propName: "default prop value"
+    to: "/404/",
+    text: "Link Text Missing",
 }
 
 InLink.propTypes = {
-    // propName: "PropTypes.string.isRequired"
+    to: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
 }
 
 export default InLink
--- src/pages/components.js
+++ src/pages/components.js
@@ -6,6 +6,7 @@
 import React from "react"
 import MainLayout from "../04_templates/main-layout-template"
 import ExLink from "../01_atoms/ExLink"
+import InLink from "../01_atoms/InLink"
 
 const ComponentsPage = () => (
     <>
@@ -13,15 +14,25 @@ const ComponentsPage = () => (
             <section className="default-page-layout__grid">
                 <div className="default-page-layout__column">
                     <h1>Components Demo Page</h1>
+
                     <h2>Atom Components</h2>
+
                     <h3>Links</h3>
+
                     <h4>External Link Working</h4>
                     <ExLink
                         href="https://github.com/nikkipantony/gatsby-starter-sass-bem-md"
-                        text="External Link"
+                        text="Ally Starter Kit on GitHub"
                     />
+
                     <h4>External Link Broken</h4>
                     <ExLink />
+
+                    <h4>Internal Link Working</h4>
+                    <InLink to="/" text="Home" />
+
+                    <h4>Internal Link Broken</h4>
+                    <InLink />
                 </div>
             </section>
         </MainLayout>
