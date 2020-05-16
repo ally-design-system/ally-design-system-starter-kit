5432479b1c98fc4aae1f3c2f981be94c111ac2d5
Nikki Pantony <nikkipantony@gmail.com>
2020-05-16 21:20:23 +0100

    Add SVG icon sprite, Add icon component

--- src/pages/style-guide.js
+++ src/pages/style-guide.js
@@ -7,6 +7,7 @@ import React from "react"
 import MainLayout from "../04_templates/main-layout-template"
 import ExLink from "../01_atoms/ExLink"
 import InLink from "../01_atoms/InLink"
+import Icon from "../01_atoms/icons/Icons"
 
 const ComponentsPage = () => (
     <>
@@ -14,25 +15,20 @@ const ComponentsPage = () => (
             <section className="default-page-layout__grid">
                 <div className="default-page-layout__column">
                     <h1>Ally Style Guide</h1>
-
                     <h2>Atom Components</h2>
-
                     <h3>Links</h3>
-
                     <h4>External Link Working</h4>
                     <ExLink
                         href="https://github.com/nikkipantony/gatsby-starter-sass-bem-md"
                         text="Ally Starter Kit on GitHub"
                     />
-
                     <h4>External Link Broken</h4>
                     <ExLink />
-
                     <h4>Internal Link Working</h4>
                     <InLink to="/" text="Home" />
-
                     <h4>Internal Link Broken</h4>
                     <InLink />
+                    <Icon symbol="fish" />
                 </div>
             </section>
         </MainLayout>
