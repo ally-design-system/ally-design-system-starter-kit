67fa10b2194f50242659a22e75632694966589ab
Nikki Pantony <nikkipantony@gmail.com>
2020-06-25 19:20:15 +0100

    Add Header Skip Link, Add Footer Skip Link, Add Main Content Component for Skip Links, Add Nav Links to Footer

--- src/03_organisms/Footer.js
+++ src/03_organisms/Footer.js
@@ -9,6 +9,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import ExLink from "../02_molecules/ExLink"
+import NavLinks from "../02_molecules/NavLinks"
 
 const Footer = () => {
     const data = useStaticQuery(graphql`
@@ -24,12 +25,12 @@ const Footer = () => {
     return (
         <footer className="o-footer">
             <div className="o-footer__inner">
+                <div className="o-footer__navigation">
+                    <NavLinks />
+                </div>
                 <p className="o-footer__text">
                     &copy; {new Date().getFullYear()}{" "}
-                    {data.site.siteMetadata.title}
-                </p>
-                <p className="o-footer__text">
-                    Built by{" "}
+                    {data.site.siteMetadata.title} &nbsp; | &nbsp; Built by{" "}
                     <ExLink
                         class="o-footer__text --ex-link"
                         href="https://nikkipantony.com"
