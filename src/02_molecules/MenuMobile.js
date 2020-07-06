8c52e55b1c56ee8a4f74add16aaf581cab4439b2
Nikki Pantony <nikkipantony@gmail.com>
2020-07-06 14:13:14 +0100

    Add toggled icon to mobile menu

--- src/02_molecules/MenuMobile.js
+++ src/02_molecules/MenuMobile.js
@@ -7,8 +7,9 @@
 import React from "react"
 import { Menu, MenuList, MenuButton, MenuLink } from "@reach/menu-button"
 import { Link } from "gatsby"
+import Icon from "../01_atoms/Icon"
 
-/* Customized `MenuLink` to work as `NavLink` component used in Desktop Menu with styled active page link in navigation */
+// Customized `MenuLink` to work as `NavLink` component used in Desktop Menu with styled active page link in navigation
 const isActive = ({ isCurrent }) => {
     return { className: isCurrent ? "active" : "navlink" }
 }
@@ -17,28 +18,40 @@ const NavLink = props => <MenuLink getProps={isActive} {...props} />
 
 const MenuMobile = () => {
     return (
-        <>
-            <Menu>
-                <MenuButton>Menu</MenuButton>
-                <MenuList className="slide-down">
-                    <NavLink as={Link} to="/">
-                        Home
-                    </NavLink>
-                    <NavLink as={Link} to="/gatsby/">
-                        Gatsby
-                    </NavLink>
-                    <NavLink as={Link} to="/sass-bem/">
-                        SASS + BEM
-                    </NavLink>
-                    <NavLink as={Link} to="/markdown/">
-                        Markdown
-                    </NavLink>
-                    <NavLink as={Link} to="/style-guide/">
-                        Style Guide
-                    </NavLink>
-                </MenuList>
-            </Menu>
-        </>
+        <Menu>
+            {({ isExpanded }) => (
+                <>
+                    <MenuButton>
+                        Menu
+                        <Icon
+                            id={isExpanded ? `close` : `burger-menu`}
+                            title={
+                                isExpanded
+                                    ? `close navigation menu`
+                                    : `open navigation menu`
+                            }
+                        />
+                    </MenuButton>
+                    <MenuList className="slide-down">
+                        <NavLink as={Link} to="/">
+                            Home
+                        </NavLink>
+                        <NavLink as={Link} to="/gatsby/">
+                            Gatsby
+                        </NavLink>
+                        <NavLink as={Link} to="/sass-bem/">
+                            SASS + BEM
+                        </NavLink>
+                        <NavLink as={Link} to="/markdown/">
+                            Markdown
+                        </NavLink>
+                        <NavLink as={Link} to="/style-guide/">
+                            Style Guide
+                        </NavLink>
+                    </MenuList>
+                </>
+            )}
+        </Menu>
     )
 }
 
