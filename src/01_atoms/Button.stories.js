0b1ffda0f0e0f37ec4217ee5f6ed0032234ceda4
Nikki Pantony <nikkipantony@gmail.com>
2020-07-07 18:01:07 +0100

    Change `src` to stories directory, Add styling with `preview.js`, Add button story

---
+++ src/01_atoms/Button.stories.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Button from "../01_atoms/Button"
+
+export default {
+    title: "Button",
+    component: Button,
+}
+
+export const DownloadText = () => <Button text="Download" />
+
+export const EmojisText = () => <Button text="😀 😎 👍 💯" />
+
+export const TextMissing = () => <Button />
+
+export const TextEmpty = () => <Button text="" />
