3a7b036aca1151bea3331c8260294b17fd52e207
Nikki Pantony <nikkipantony@gmail.com>
2020-07-08 20:55:49 +0100

    Add `Icon` story

---
+++ src/01_atoms/Icon.stories.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Icon from "../01_atoms/Icon"
+
+export default {
+    title: "/01 Atoms/Icon",
+    component: Icon,
+}
+
+export const OpenInNew = () => (
+    <Icon
+        id="open-in-new"
+        title="Link will open in new
+                    tab"
+    />
+)
