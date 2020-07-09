f9b7f8e946f254074f3d17d96e265a869c63411a
Nikki Pantony <nikkipantony@gmail.com>
2020-07-09 17:14:32 +0100

    Add `SkipLink` story to storybook

---
+++ src/01_atoms/SkipLink.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import SkipLink from "../01_atoms/SkipLink"
+
+export default {
+    title: "/01 Atoms/Skip Link",
+    component: SkipLink,
+}
+
+export const Working = () => <SkipLink />
