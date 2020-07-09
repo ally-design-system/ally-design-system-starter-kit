38d6b6679336331631559e90f225e9ee331072f3
Nikki Pantony <nikkipantony@gmail.com>
2020-07-09 18:45:12 +0100

    Add `NavLink` stories to storybook

---
+++ src/01_atoms/NavLink.stories.js
@@ -0,0 +1,13 @@
+import React from "react"
+import NavLink from "../01_atoms/NavLink"
+
+export default {
+    title: "/01 Atoms/Navigation Link",
+    component: NavLink,
+}
+
+export const WithText = () => <NavLink to="/" text="Home" />
+
+export const TextMissing = () => <NavLink to="/" text="" />
+
+export const PropsMissing = () => <NavLink />
