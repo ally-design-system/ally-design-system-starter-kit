c71f26b871c7475afcf041ff66f231032ff39a9e
Nikki Pantony <nikkipantony@gmail.com>
2020-07-09 18:49:52 +0100

    Add desktop menu stories to storybook

---
+++ src/02_molecules/MenuDesktop.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import MenuDesktop from "../02_molecules/MenuDesktop"
+
+export default {
+    title: "/02 Molecules/Menu Desktop",
+    component: MenuDesktop,
+}
+
+export const Working = () => <MenuDesktop />
