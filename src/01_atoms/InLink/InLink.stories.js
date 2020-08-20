d81c09d0fcf726dbe0b3e202c5b2d3cc4d587917
Nikki Pantony <nikkipantony@gmail.com>
2020-08-20 20:23:09 +0100

    Add parameter and argType examples to InLink storybook docs

--- src/01_atoms/InLink/InLink.stories.js
+++ src/01_atoms/InLink/InLink.stories.js
@@ -9,6 +9,27 @@ import InLink from "./InLink"
 export default {
     title: "/Atoms/Internal Link",
     component: InLink,
+    parameters: {
+        docs: {
+            description: {
+                component: "Component description witten in _markdown_.",
+            },
+        },
+    },
+    argTypes: {
+        to: {
+            description: "Props description",
+        },
+        text: {
+            description: "Props description",
+        },
+        className: {
+            description: "Props description",
+        },
+        modifier: {
+            description: "Props description",
+        },
+    },
 }
 
 export const InternalLink = args => <InLink to="/" {...args} />
